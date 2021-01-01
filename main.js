aa03171e4ce47cfb4df05a291892385670c0b58c
Abdallah <abdallahchaar90@gmail.comh>
2021-01-01 16:02:45 +0100

    var

--- main.js
+++ main.js
@@ -1,10 +1,53 @@
 /* global consnle, alert*/
 
 /* 
-start With Letters, Underscore; 
+start With Letters, Underscore,
+
+var = JavaScript variable Keyword
+
+myPrice = Variable Name
+
+( = ) = Assignment Operator
+
+100 = variable Value
 */
 
 var myPrice; // Undefined
 
-myprice = 100;
+myPrice = 100;
+
+document.getElementById("price").innerHTML = myPrice;
+____________________________________
+
+var myOldPrice = 2000;
+
+var myNewPrice = 900;
+
+var myDiscount = myOldPrice - myNewPrice; // 2000 - 900 = 1100
+
+document.getElementById("price").innerHTML = myDiscount;
+
+________________
+
+var myOldPrice = 2000;
+
+var myNewPrice = 900;
+
+var myDiscount = myOldPrice - myNewPrice + 500; // 2000 - 900 + 500= 1600
+
+document.getElementById("price").innerHTML = myDiscount;
+
+___________________________
+
+myOldPrice = 2000,
+
+myNewPrice = 900,
+
+myDiscount = myOldPrice - myNewPrice + 500; // 2000 - 900 + 500= 1600
+
+document.getElementById("price").innerHTML = myDiscount;
+
+
+
+
 
